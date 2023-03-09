d854b3df969e7b4c3cfd4b7d19edc450c3efc7f4
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 04:39:56 +0530

    minor fix

--- src/utils/makeRequest/index.js
+++ src/utils/makeRequest/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { BACKEND_URL, AUTH_URL } from '../../constants/apiEndpoints';
+import { BACKEND_URL, AUTH_URL } from '../../constants/apiEndPoints';
 import { ERROR_ROUTE } from '../../constants/routes';
 
 const makeRequest = async (apiEndPoint, navigate, dynamicConfig = {}, token) => {
@@ -26,28 +26,18 @@ const makeRequest = async (apiEndPoint, navigate, dynamicConfig = {}, token) => 
   }
 };
 
-const makeAuthRequest = async (apiEndPoint, navigate, dynamicConfig = {}, token) => {
-  try {
-    const requestDetails = {
-      baseURL: AUTH_URL,
-      url: apiEndPoint.url,
-      method: apiEndPoint.method,
-      ...dynamicConfig,
-      headers: {
-        authorization: `Bearer ${token}`
-      }
-    };
-    const { data } = await axios(requestDetails);
-    return data;
-  } catch (e) {
-    const errorStatus = e.response?.status;
-    if (errorStatus) {
-      navigate(`${ERROR_ROUTE}/${errorStatus}`);
-    } else {
-      navigate(ERROR_ROUTE);
-    }
-    return null;
-  }
+const makeAuthRequest = async (apiEndPoint, dynamicConfig) => {
+
+  const requestDetails = {
+    baseURL: AUTH_URL,
+    url: apiEndPoint.url,
+    method: apiEndPoint.method,
+    ...dynamicConfig,
+
+  };
+  const { data } = await axios(requestDetails);
+  return data;
+
 };
 
 export { makeRequest, makeAuthRequest };
