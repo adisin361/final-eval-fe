5f8418894daa1db9b4f69bff7a94f07469b2fccd
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 04:42:33 +0530

    minor fix

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,8 @@ import './App.css';
 import React from 'react';
 import { LoginPage, RegisterPage, Dashboard } from './pages';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import { LOGIN_ROUTE, REGISTER_ROUTE, DASHBOARD_ROUTE } from './constants/routes';
+import { LOGIN_ROUTE, REGISTER_ROUTE, DASHBOARD_ROUTE, COLLECTIONS_ROUTE } from './constants/routes';
+import CollectionsPage from './pages/Collections';
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Route path={REGISTER_ROUTE} element={<RegisterPage />} />
           <Route path={LOGIN_ROUTE} element={<LoginPage />} />
           <Route path={DASHBOARD_ROUTE} element={<Dashboard />} />
+          <Route path={COLLECTIONS_ROUTE} element={<CollectionsPage />} />
         </Routes>
       </BrowserRouter>
     </div>
