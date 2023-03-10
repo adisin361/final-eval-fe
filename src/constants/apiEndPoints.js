dbc0fc7d8ec59343cd64955aa94872dd3a0d4f6d
Aditya Sinha <adityasinha361@gmail.com>
2023-03-10 11:22:15 +0530

    feat: add suitable api end points

--- src/constants/apiEndPoints.js
+++ src/constants/apiEndPoints.js
@@ -15,3 +15,58 @@ export const VALIDATE_TOKEN_URL = {
   url: '/token/validate',
   method: 'get'
 };
+
+export const CREATE_CONTENT_TYPE_URL = {
+  url: 'record/save',
+  method: 'post',
+};
+
+export const UPDATE_CONTENT_TYPE_URL = {
+  url: 'record/update',
+  method: 'post',
+};
+
+export const ADD_FIELD_CONTENT_URL = {
+  url: 'record/field',
+  method: 'post',
+};
+
+export const EDIT_FIELD_CONTENT_URL = {
+  url: 'record/save',
+  method: 'patch',
+};
+
+export const DELETE_FIELD_CONTENT_URL = {
+  url: 'record/save',
+  method: 'delete',
+};
+
+export const GET_CONTENT_DETAIL_URL = {
+  url: 'record/details',
+  method: 'get',
+};
+
+export const GET_COLLECTION_DETAILS = {
+  url: 'collection/details',
+  method: 'get',
+};
+
+export const GET_COLLECTION_DATA = {
+  url: 'collection/details',
+  method: 'post'
+};
+
+export const DELETE_COLLECTION_ENTRY = {
+  url: 'collection/field',
+  method: 'delete'
+};
+
+export const EDIT_COLLECTION_ENTRY = {
+  url: 'collection/update',
+  method: 'patch'
+};
+
+export const ADD_COLLECTION_ENTRY = {
+  url: 'collection/update',
+  method: 'post'
+};
